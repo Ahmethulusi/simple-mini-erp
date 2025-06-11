f61239022a1a93748a34ec7d3cfeee4f7539803c
ahulusi_ymk <ahmet4112004@gmail.com>
2025-06-11 16:43:29 +0300

    db güncellendi

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,13 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-   
-});
-
-
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Render cloud DB için gerekli!
+  }
+})
 
 module.exports = pool;
